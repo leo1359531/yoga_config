a468bd0fddb3704f880934dbca637addc41e55c9
leo1359531 <leos1359531@gmail.com>
2022-10-13 12:37:15 +0200

    Thu Oct 13 12:37:15 PM CEST 2022

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -17,23 +17,25 @@ set.shiftwidth = 4
 
 -- hdl-checker
 -- Only define once
-if not require'lspconfig.configs'.hdl_checker then
-  require'lspconfig.configs'.hdl_checker = {
-    default_config = {
-    cmd = {"hdl_checker", "--lsp", };
-    filetypes = {"vhdl", "verilog", "systemverilog"};
-      root_dir = function(fname)
-        -- will look for the .hdl_checker.config file in parent directory, a
-        -- .git directory, or else use the current directory, in that order.
-        local util = require'lspconfig'.util
-        return util.root_pattern('.hdl_checker.config')(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
-      end;
-      settings = {};
-    };
-  }
-end
+-- if not require'lspconfig.configs'.hdl_checker then
+--   require'lspconfig.configs'.hdl_checker = {
+--     default_config = {
+--     cmd = {"hdl_checker", "--lsp", };
+--     filetypes = {"vhdl", "verilog", "systemverilog"};
+--       root_dir = function(fname)
+--         -- will look for the .hdl_checker.config file in parent directory, a
+--         -- .git directory, or else use the current directory, in that order.
+--         local util = require'lspconfig'.util
+--         return util.root_pattern('.hdl_checker.config')(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+--       end;
+--       settings = {};
+--     };
+--   }
+-- end
 
-require'lspconfig'.hdl_checker.setup{}
+-- require'lspconfig'.hdl_checker.setup{}
+
+require'lspconfig'.ghdl_ls.setup{}
 
 require'lspconfig'.bashls.setup{}
 
